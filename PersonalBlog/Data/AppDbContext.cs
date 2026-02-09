984e1f4e0f7a3688a17d1a091c64d01d696812b3
indra <jossepa97@gmail.com>
2026-02-08 22:17:55 -0500

    Add project files.

---
+++ PersonalBlog/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalBlog.Models;
+namespace PersonalBlog.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(tb =>
+            {
+                tb.HasKey(col => col.IdUser);
+                tb.Property(col => col.IdUser)
+                .UseIdentityColumn()
+                .ValueGeneratedOnAdd();
+                tb.Property(col => col.name).HasMaxLength(50);
+                tb.Property(col => col.password).HasMaxLength(255);
+                tb.Property(col => col.description).HasMaxLength(255);
+            });
+            modelBuilder.Entity<User>().ToTable("User");
+        }
+    }
+}
---
+++ PersonalBlog/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalBlog.Models
+{
+    public class User
+    {
+        public int IdUser  { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public string description { get; set; }
+
+    }
+}
